30d8f96aad31270ee20e2e6c1ed2a29fc94db904
limalai <124987652+limalai@users.noreply.github.com>
2023-10-10 17:25:13 +0700

    Uoload file
    new Test case

--- Lab13_SumOfDigit/src/main/SumOfDigits.java
+++ Lab13_SumOfDigit/src/main/SumOfDigits.java
@@ -2,12 +2,9 @@ package main;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class SumOfDigits {
-    public static int sumOfDigits(int number) {
-        return number;
+    public SumOfDigits(int number) {
+    	
     }
-
 }
-
--- Lab13_SumOfDigit/src/test/SumOfDigitsTest.java
+++ Lab13_SumOfDigit/src/test/SumOfDigitsTest.java
@@ -1,26 +1,41 @@
 package test;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.Assert.assertEquals;
 import main.SumOfDigits;
 
-public class SumOfDigitsTest {
+public class SumOfDigitsTest {	
+	SumOfDigits num1;
+	SumOfDigits num2;
+	SumOfDigits num3;
+	SumOfDigits num4;
+	SumOfDigits num5;
+	SumOfDigits num6;
+	SumOfDigits num7;
+	SumOfDigits num8;
+	
+	@BeforeEach
+	public void setup() {
+		num1 = new SumOfDigits(26);
+		num2 = new SumOfDigits(157);
+		num3 = new SumOfDigits(260);
+		num4 = new SumOfDigits(057);
+		num5 = new SumOfDigits(05);
+		num6 = new SumOfDigits(0);
+		num7 = new SumOfDigits(1);
+		num8 = new SumOfDigits(9);
+	}
 
     @Test
     public void testSumOfDigits() {
-        // Test case 1: Test with 493193
-        int result1 = SumOfDigits.sumOfDigits(5678);
-        assertEquals(9, result1);
-
-        // Test case 2: Test with 987654321
-        int result2 = SumOfDigits.sumOfDigits(21456);
-        assertEquals(3, result2);
-
-        // Test case 3: Test with 0
-        int result3 = SumOfDigits.sumOfDigits(22544);
-        assertEquals(4, result3);
-
-        // Test case 4: Test with a negative number (-12345)
-        int result4 = SumOfDigits.sumOfDigits(-56712);
-        assertEquals(3, result4);
+        assertEquals(8, num1);
+        assertEquals(4, num2);
+        assertEquals(8, num3);
+        assertEquals(1, num4);
+        assertEquals(5, num5);
+        assertEquals("ตัวเลขต้องมีสองหลัก", num6);
+        assertEquals("ตัวเลขต้องมีสองหลัก", num7);
+        assertEquals("ตัวเลขต้องมีสองหลัก", num8);
     }
 }
