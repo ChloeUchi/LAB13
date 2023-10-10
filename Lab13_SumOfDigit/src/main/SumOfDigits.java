fafaf04ad0ce1c22fa8aecf7350a12f4762d42a9
limalai <124987652+limalai@users.noreply.github.com>
2023-10-10 13:58:10 +0700

    Upload file

---
+++ Lab13_SumOfDigit/src/main/SumOfDigits.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static int sumOfDigits(int number) {
+        return number;
+    }
+
+}
+
