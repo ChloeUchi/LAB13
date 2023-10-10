2a95811ac3e58887ce649222f7bd2bfa5e8fe9bc
limalai <limalai.p@kkumail.com>
2023-10-10 18:39:10 +0700

    Update Junit test

--- Lab13_SumOfDigit/src/test/SumOfDigitsTest.java
+++ Lab13_SumOfDigit/src/test/SumOfDigitsTest.java
@@ -1,41 +1,39 @@
 package test;
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
-
-import static org.junit.Assert.assertEquals;
 import main.SumOfDigits;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SumOfDigitsTest {	
-	SumOfDigits num1;
-	SumOfDigits num2;
-	SumOfDigits num3;
-	SumOfDigits num4;
-	SumOfDigits num5;
-	SumOfDigits num6;
-	SumOfDigits num7;
-	SumOfDigits num8;
-	
-	@BeforeEach
-	public void setup() {
-		num1 = new SumOfDigits(26);
-		num2 = new SumOfDigits(157);
-		num3 = new SumOfDigits(260);
-		num4 = new SumOfDigits(057);
-		num5 = new SumOfDigits(05);
-		num6 = new SumOfDigits(0);
-		num7 = new SumOfDigits(1);
-		num8 = new SumOfDigits(9);
-	}
+
+public class SumOfDigitsTest {
 
     @Test
-    public void testSumOfDigits() {
-        assertEquals(8, num1);
-        assertEquals(4, num2);
-        assertEquals(8, num3);
-        assertEquals(1, num4);
-        assertEquals(5, num5);
-        assertEquals(0, num6);
-        assertEquals(0, num7);
-        assertEquals(0, num8);
+    public void testComputeSumOfDigits_ErrorForSingleDigit() {
+        SumOfDigits sod = new SumOfDigits();
+        String result = sod.computeSumOfDigits(5);
+        assertEquals("ตัวเลขต้องมีสองหลัก", result);
+    }
+
+    @Test
+    public void testComputeSumOfDigits_ValidNumber() {
+        SumOfDigits sod = new SumOfDigits();
+        String result = sod.computeSumOfDigits(123);
+        String expected = "1 + 2 + 3 = 6\n6";
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testComputeSumOfDigits_NumberWithMultipleReductions() {
+        SumOfDigits sod = new SumOfDigits();
+        String result = sod.computeSumOfDigits(999);
+        String expected = "9 + 9 + 9 = 27\n2 + 7 = 9\n9";
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testComputeSumOfDigits_TwoDigitNumber() {
+        SumOfDigits sod = new SumOfDigits();
+        String result = sod.computeSumOfDigits(47);
+        String expected = "4 + 7 = 11\n1 + 1 = 2\n2";
+        assertEquals(expected, result);
     }
 }
